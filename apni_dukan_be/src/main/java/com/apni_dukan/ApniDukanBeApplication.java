aff76591c0b8909f427875bed7215aafcf67cc81
Bhasker23 <shrimalibhaskar2398@gmail.com>
2023-04-11 16:48:57 +0530

    Folder Structure completed

---
+++ apni_dukan_be/src/main/java/com/apni_dukan/ApniDukanBeApplication.java
@@ -0,0 +1,13 @@
+package com.apni_dukan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApniDukanBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApniDukanBeApplication.class, args);
+	}
+
+}
---
+++ apni_dukan_be/src/test/java/com/apni_dukan/ApniDukanBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apni_dukan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApniDukanBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
